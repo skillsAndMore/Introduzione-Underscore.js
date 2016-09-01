d3cd08de6501f1244fb187323d85513782af43bb
Andrea Barghigiani <a.barghigiani@gmail.com>
2016-09-01 17:34:15 +0200

    each in Underscore.js

--- js/und-test.js
+++ js/und-test.js
@@ -7,8 +7,15 @@
 // console.log(sopraLimite);
 
 //2. Classico ciclo for
+// var linguaggi = [ 'PHP', 'JavaScript', 'CSS', 'HTML', 'Ruby', 'Go' ];
+//
+// for( var i = 0; i < linguaggi.length; i++ ){
+//     console.log( 'Linguaggio: ' + linguaggi[i] );
+// }
+
+//3. Trasformazione ciclo for con each
 var linguaggi = [ 'PHP', 'JavaScript', 'CSS', 'HTML', 'Ruby', 'Go' ];
 
-for( var i = 0; i < linguaggi.length; i++ ){
-    console.log( 'Linguaggio: ' + linguaggi[i] );
-}
+_.each( linguaggi, function( linguaggio, index, linguaggi ) {
+    console.log( 'Linguaggio: ' + linguaggio + ' con indice(' + index + ')' );
+});
